d6ab496d89d81332dd12ca2cb3eb6357eccbe41e
Raphael Silva <rapphil@gmail.com>
2023-01-28 19:59:09 +0000

    Fix workflow

--- .github/scripts/patch.sh
+++ .github/scripts/patch.sh
@@ -9,11 +9,11 @@ PATCH_VERSION=1.21.1-adot
 mkdir tmp
 cd tmp
 
-if [[ -f ../patchs/opentelemetry-java.patch ]]; then
+if [[ -f .github/patchs/opentelemetry-java.patch ]]; then
 git clone https://github.com/open-telemetry/opentelemetry-java.git
 cd opentelemetry-java
 git checkout v${OTEL_JAVA_VERSION} -b tag-v${OTEL_JAVA_VERSION}
-patch -p1 < ../../patchs/opentelemetry-java.patch
+patch -p1 < .github/patchs/opentelemetry-java.patch
 
 git commit -a -m "Patched version ${PATCH_VERSION}"
 
@@ -24,13 +24,13 @@ else
 echo "Skiping patching opentelemetry-java"
 fi
 
-if [[ -f ../patchs/opentelemetry-java-instrumentation.patch]]; then
+if [[ -f .github/patchs/opentelemetry-java-instrumentation.patch]]; then
 git clone https://github.com/open-telemetry/opentelemetry-java-instrumentation.git
 
 cd opentelemetry-java-instrumentation
 git checkout v${OTEL_JAVA_INSTRUMENTATION_VERSION} -b tag-v${OTEL_JAVA_INSTRUMENTATION_VERSION}
 
-patch -p1 < ../../patchs/opentelemetry-java-instrumentation.patch
+patch -p1 < .github/patchs/opentelemetry-java-instrumentation.patch
 
 git commit -a -m "Patched version ${PATCH_VERSION}"
 
